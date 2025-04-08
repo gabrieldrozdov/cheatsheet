af92c2d6af5d0940806d2c9c29a63eb896228c68
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2025-04-08 07:55:26 -0400

    js and new layout

--- script.js
+++ script.js
@@ -64,3 +64,32 @@ for (let section of document.querySelectorAll('.section')) {
 	section.dataset.active = 0;
 	transitionObserverSections.observe(section);
 }
+
+// Header animation
+function animateHeader() {
+	const headerAnimation = document.querySelector('.header-animation');
+	const circle = document.createElement('circle');
+	circle.classList.add('header-circle');
+	circle.style.top = `${Math.random()*100}%`;
+	circle.style.left = `${Math.random()*100}%`;
+	circle.style.width = `${Math.random()*50+25}vw`;
+	circle.style.height = `${Math.random()*50+25}vw`;
+	circle.style.opacity = `0`;
+	circle.style.setProperty('--primary', `var(--${colors[Math.floor(Math.random()*colors.length)]})`);
+	circle.style.transform = `translate3d(-50%, -50%, 0) scale3d(.8, .8, 1) rotate(${Math.random()*360-180}deg)`;
+	headerAnimation.append(circle);
+	setTimeout(() => {
+		circle.style.transform = `translate3d(${Math.random()*100-100}%, ${Math.random()*100-200}%, 0) scale3d(1, 1, 1) rotate(${Math.random()*360-180}deg)`;
+		circle.style.opacity = `1`;
+	}, 50)
+	setTimeout(() => {
+		circle.style.opacity = `0`;
+	}, 5000)
+	setTimeout(() => {
+		circle.remove();
+	}, 10000)
+}
+animateHeader();
+animateHeader();
+setInterval(animateHeader, 2000);
+setInterval(animateHeader, 2000);
